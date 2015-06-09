d93be11533008df221b6575380ecf11dfcbe2798
amunsill <alex.m987@gmail.com>
2015-06-08 23:39:38 -0500

    Finished setting up gems

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-# Default URL
-config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
